e01c2c3b0037eba05e23b6e052eda8d549956197
kojix2 <2xijok@gmail.com>
2024-12-18 00:26:54 +0900

    Refactor: Move `format_time_span` method to utils module

--- src/checksum/app/check.cr
+++ src/checksum/app/check.cr
@@ -213,16 +213,5 @@ module CheckSum
 
       puts
     end
-
-    private def format_time_span(span : Time::Span)
-      total_seconds = span.total_seconds
-      if total_seconds < 60
-        return "#{total_seconds.round(2)} seconds"
-      end
-
-      minutes = span.total_minutes
-      seconds = span.seconds
-      "#{"%d" % minutes.floor}:#{seconds < 10 ? "0" : ""}#{seconds} minutes"
-    end
   end
 end
--- src/checksum/app/utils.cr
+++ src/checksum/app/utils.cr
@@ -18,5 +18,16 @@ module CheckSum
       formatted_index = (index + 1).to_s.rjust(total_digits, ' ')
       "(#{formatted_index}/#{total}) "
     end
+
+    private def format_time_span(span : Time::Span)
+      total_seconds = span.total_seconds
+      if total_seconds < 60
+        return "#{total_seconds.round(2)} seconds"
+      end
+
+      minutes = span.total_minutes
+      seconds = span.seconds
+      "#{"%d" % minutes.floor}:#{seconds < 10 ? "0" : ""}#{seconds} minutes"
+    end
   end
 end
