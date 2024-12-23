2231f0bdb5ee16b46e94ea2da9264e3b7db4a564
kojix2 <2xijok@gmail.com>
2024-12-23 15:44:13 +0900

    Make it compilable when git is not available

--- src/checksum/version.cr
+++ src/checksum/version.cr
@@ -1,4 +1,5 @@
 module CheckSum
   VERSION = {{ `shards version #{__DIR__}`.chomp.stringify }}
-  SOURCE  = {{ `git remote get-url origin`.chomp.stringify }}
+  SOURCE  = "https://github.com/kojix2/checksum.cr"
+  # SOURCE  = {{ `git remote get-url origin`.chomp.stringify }}
 end
