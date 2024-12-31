a77e9563dcd6273a6ffb921db144122d066ed55a
kojix2 <2xijok@gmail.com>
2024-12-31 14:35:56 +0900

    Refactor: Extract method for default screen width retrieval

--- src/checksum/ioctl.cr
+++ src/checksum/ioctl.cr
@@ -13,28 +13,32 @@
 {% end %}
 
 module CheckSum
+  # Returns the width of the terminal screen.
+  # It first checks the `COLUMNS` environment variable.
+  # If not set, it defaults to `80`.
+  private def self.default_screen_width : Int32
+    ENV.fetch("COLUMNS", 80).to_i
+  end
+
+  # Returns the width of the terminal screen.
+  # - On Unix systems, it uses the `ioctl` system call to get the screen width.
+  # - On other systems, it checks the `COLUMNS` environment variable.
+  #   - If not set, it defaults to `80`.
+
   {% if flag?(:unix) %}
-    def self.screen_width
+    def self.screen_width : Int32
       ws = LibC::Winsize.new
       fd = STDOUT.fd
 
       if LibC.ioctl(fd, LibC::TIOCGWINSZ, pointerof(ws)) == 0
         ws.ws_col.to_i32
       else
-        begin
-          ENV.fetch("COLUMNS", 80).to_i
-        rescue
-          80
-        end
+        default_screen_width
       end
     end
   {% else %}
     def self.screen_width : Int32
-      begin
-        ENV.fetch("COLUMNS", 80).to_i
-      rescue
-        80
-      end
+      default_screen_width
     end
   {% end %}
 end
