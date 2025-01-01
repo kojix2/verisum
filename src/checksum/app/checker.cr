0f4b57cce2a028f0eca297532586e42bc49b2945
kojix2 <2xijok@gmail.com>
2025-01-01 17:18:16 +0900

    Improve parse_lines method to handle BOM and streamline line parsing

--- src/checksum/app/checker.cr
+++ src/checksum/app/checker.cr
@@ -63,9 +63,21 @@ module CheckSum
       end
 
       private def parse_lines(io : IO) : Array(FileRecord)
-        io.each_line.map do |line|
-          parse_line(line)
-        end.to_a.compact
+        result = Array(FileRecord?).new
+
+        # Get the first line
+        first_line = io.gets
+        return result.compact unless first_line
+
+        # Remove the BOM if it exists
+        first_line = first_line[1..-1] if first_line.starts_with?("\xEF\xBB\xBF")
+        result << parse_line(first_line)
+
+        io.each_line do |line|
+          result << parse_line(line)
+        end
+
+        result.compact
       end
 
       private def parse_line(line : String) : FileRecord?
