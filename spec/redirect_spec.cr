a8565101b5ad264c93ded008085d576d4116c473
kojix2 <2xijok@gmail.com>
2025-01-07 22:59:02 +0900

    Add tests for CheckSum::Redirect

---
+++ spec/redirect_spec.cr
@@ -0,0 +1,81 @@
+require "spec"
+require "../src/checksum/redirect"
+
+class TestObject
+  include CheckSum::Redirect
+
+  def public_print(*args)
+    print(*args)
+  end
+
+  def public_puts(*args)
+    puts(*args)
+  end
+end
+
+describe CheckSum::Redirect do
+  it "has a default stdout" do
+    redirect = TestObject.new
+    redirect.stdout.should eq(STDOUT)
+  end
+
+  it "has a default stderr" do
+    redirect = TestObject.new
+    redirect.stderr.should eq(STDERR)
+  end
+
+  it "redirects print to custom stdout" do
+    io = IO::Memory.new
+    redirect = TestObject.new
+    redirect.stdout = io
+
+    redirect.public_print "Hello, world!"
+    io.rewind
+
+    io.gets(chomp: false).should eq("Hello, world!")
+  end
+
+  it "redirects puts to custom stdout" do
+    io = IO::Memory.new
+    redirect = TestObject.new
+    redirect.stdout = io
+
+    redirect.public_puts "Hello, world!"
+    io.rewind
+
+    io.gets(chomp: false).should eq("Hello, world!\n")
+  end
+
+  it "redirects print to custom stderr" do
+    io = IO::Memory.new
+    redirect = TestObject.new
+    redirect.stderr = io
+
+    redirect.stderr.print "Error message"
+    io.rewind
+
+    io.gets(chomp: false).should eq("Error message")
+  end
+
+  it "handles empty strings in print" do
+    io = IO::Memory.new
+    redirect = TestObject.new
+    redirect.stdout = io
+
+    redirect.public_print ""
+    io.rewind
+
+    io.gets.should be_nil
+  end
+
+  it "handles multiple arguments in print" do
+    io = IO::Memory.new
+    redirect = TestObject.new
+    redirect.stdout = io
+
+    redirect.public_print "Hello", ", ", "world!"
+    io.rewind
+
+    io.gets(chomp: false).should eq("Hello, world!")
+  end
+end
