ae43360e09e5de1dfd73e001cf2182b5ff71f0ce
kojix2 <2xijok@gmail.com>
2025-01-07 23:19:09 +0900

    Add test for help message in CheckSum::Parser

--- spec/parser_spec.cr
+++ spec/parser_spec.cr
@@ -8,4 +8,17 @@ describe CheckSum::Parser do
       parser.parse([] of String)
     end
   end
+
+  it "builds a help message" do
+    parser = CheckSum::Parser.new
+    io = IO::Memory.new
+    parser.help_message(io)
+    io.rewind
+    msg = io.gets_to_end
+    msg.should contain("Program:")
+    msg.should contain("Version:")
+    msg.should contain("Usage:")
+    msg.should contain("-N, --no-clear")
+    msg.should contain("-h, --help")
+  end
 end
