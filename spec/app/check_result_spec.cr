8a337be6fd4dcc7ea759560cd062930f5643eb58
kojix2 <2xijok@gmail.com>
2025-01-08 20:38:06 +0900

    Rename `result.cr` to `check_result.cr` and add tests

---
+++ spec/app/check_result_spec.cr
@@ -0,0 +1,80 @@
+require "spec"
+require "../../src/checksum/app/check_result"
+
+describe CheckSum::App::CheckResult do
+  it "initializes with default values" do
+    result = CheckSum::App::CheckResult.new
+    result.total.should eq 0
+    result.success.should eq 0
+    result.mismatch.should eq 0
+    result.error.should eq 0
+  end
+
+  it "initializes with given values" do
+    result = CheckSum::App::CheckResult.new(10, 5, 3, 2)
+    result.total.should eq 10
+    result.success.should eq 5
+    result.mismatch.should eq 3
+    result.error.should eq 2
+  end
+
+  it "converts to hash" do
+    result = CheckSum::App::CheckResult.new(10, 5, 3, 2)
+    result.to_h.should eq({total: 10, success: 5, mismatch: 3, error: 2})
+  end
+
+  it "formats to string" do
+    result = CheckSum::App::CheckResult.new(10, 5, 3, 2)
+    r = result.to_s
+    r.should contain("10 files")
+    r.should contain("5 successes")
+    r.should contain("3 mismatches")
+    r.should contain("2 errors")
+  end
+
+  it "formats to string with singular" do
+    result = CheckSum::App::CheckResult.new(1, 1, 1, 1)
+    r = result.to_s
+    r.should contain("1 file")
+    r.should contain("1 success")
+    r.should contain("1 mismatch")
+    r.should contain("1 error")
+  end
+
+  it "formats to string with zero" do
+    result = CheckSum::App::CheckResult.new(0, 0, 0, 0)
+    r = result.to_s
+    r.should contain("0 files")
+    r.should contain("0 successes")
+    r.should contain("0 mismatches")
+    r.should contain("0 errors")
+  end
+
+  it "formats to string with color" do
+    result = CheckSum::App::CheckResult.new(10, 5, 3, 2)
+    r = result.to_s
+    r.should contain("5 successes".colorize(:green).to_s)
+    r.should contain("3 mismatches".colorize(:red).to_s)
+    r.should contain("2 errors".colorize(:magenta).to_s)
+  end
+
+  it "formats to string without color" do
+    result = CheckSum::App::CheckResult.new(0, 0, 0, 0)
+    r = result.to_s
+    r.should contain("0 files")
+    r.should contain("0 successes")
+    r.should contain("0 mismatches")
+    r.should contain("0 errors")
+    r.should_not contain("0 successes".colorize(:green).to_s)
+    r.should_not contain("0 mismatches".colorize(:red).to_s)
+    r.should_not contain("0 errors".colorize(:magenta).to_s)
+  end
+
+  it "compares equality" do
+    result1 = CheckSum::App::CheckResult.new(10, 5, 3, 2)
+    result2 = CheckSum::App::CheckResult.new(10, 5, 3, 2)
+    result3 = CheckSum::App::CheckResult.new(1, 1, 1, 1)
+    result1.should eq result2
+    result1.should_not eq result3
+  end
+end
